9c940fbbfbead1b0c39cacc384b0ce86c59e687c
jravolio <jravolio892@gmail.com>
2022-06-14 10:28:52 -0300

    notebook commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
 import logoImg from '../../assets/logo.svg'
 import { Container,Content } from './styles' // Container e Content são os componente de estilização
+import Modal from 'react-modal'
+import { useState } from 'react'
 
 
 export function Header(){
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dtmoney" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
